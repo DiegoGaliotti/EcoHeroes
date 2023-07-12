2e9f2340d54ef265ace45aefd1f653f8d235d72c
Diego Galiotti <diegaliotti@gmail.com>
2023-07-11 21:08:56 -0300

    CurrencySystem Script finished

--- Assets/Scripts/World3dScripts/Event Manager/CurrencySystem.cs
+++ Assets/Scripts/World3dScripts/Event Manager/CurrencySystem.cs
@@ -1,17 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CurrencySystem : MonoBehaviour
 {
-   private static Dictionary<CurrencyType, int> currencyAmount = new Dictionary<CurrencyType, int>();
+    private static Dictionary<CurrencyType, int> CurrencyAmounts = 
+        new Dictionary<CurrencyType, int>(); //Creamos una variable CurrencyAmount que es un diccionario, del tipo de currency (coin o gold) y la cantidad
 
-    [SerializeField] private List<GameObject> text;
-    
-    private Dictionary<CurrencyType, TextMeshProUGUI> currencyText = new Dictionary<CurrencyType, TextMeshProUGUI>();
-    
+    [SerializeField] private List<GameObject> texts; //Creo una lista de textos
+   
+    private Dictionary<CurrencyType, TextMeshProUGUI> currencyTexts = 
+        new Dictionary<CurrencyType, TextMeshProUGUI>(); //Creamos una variable CurrencyAmount que es un diccionario, del tipo de currency (coin o gold) y el texto
 
+    private void Awake() //Cuando arranque el juego va a pasar lo siguiente:
+    {
+        for (int i = 0; i < texts.Count; i++) //Para todos los gameobject dentro de la lista textos
+        {
+            CurrencyAmounts.Add((CurrencyType)i, 0); //Va a agregar un el tipo de currency y un valor puesto a mano que es 0
+            currencyTexts.Add((CurrencyType)i, texts[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>()); //en el currency text va a poner el tecto de la lista
+        }
+    }
 
+    private void Start()
+    {
+        EventManager.Instance.AddListener<CurrencyChangeGameEvent>(OnCurrencyChange);
+        EventManager.Instance.AddListener<NotEnoughCurrencyGameEvent>(OnNotEnough); 
+    }
+
+    private void OnCurrencyChange(CurrencyChangeGameEvent info)
+    {
+        //To do save the currency
+        CurrencyAmounts[info.currencyType] += info.amount;
+        currencyTexts[info.currencyType].text = CurrencyAmounts[info.currencyType].ToString();
+    }
+
+    private void OnNotEnough(NotEnoughCurrencyGameEvent info) // Cuando no tenga más currency
+    {
+        Debug.Log(message: $"You don´t have enough of {info.amount} {info.currencyType}"); //Te tira el error, de la cantidad y del tipo
+    }
 
 }
 
